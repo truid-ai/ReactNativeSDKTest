dd21f21b895c5f194dd18ad1e10aea039069e2f8
Hamza <muhammadhamzam1486@gmail.com>
2024-07-29 16:32:49 +0500

    complete for nadra sdk

---
+++ App.jsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+
+import Home from './Home';
+import Complete from './Complete';
+
+function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Complete" component={Complete} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ ios/calendarModuleBridge.m
@@ -0,0 +1,8 @@
+//
+//  calendarModuleBridge.m
+//  AwesomeProject
+//
+//  Created by truID on 08/07/2024.
+//
+
+#import <Foundation/Foundation.h>
